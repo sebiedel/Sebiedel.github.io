588e23b91c52440e37f17565fcf5a0c529439e5d
sebiedel <33039896+sebiedel@users.noreply.github.com>
2017-10-24 07:56:48 +0200

    Initial commit

---
+++ My Project/AssemblyInfo.vb
@@ -0,0 +1,34 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' Allgemeine Informationen über eine Assembly werden über die folgenden 
+' Attribute gesteuert. Ändern Sie diese Attributwerte, um die
+' Assemblyinformationen zu ändern.
+
+' Werte der Assemblyattribute überprüfen
+<Assembly: AssemblyTitle("WebApplication9")>
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyCompany("HP Inc.")>
+<Assembly: AssemblyProduct("WebApplication9")>
+<Assembly: AssemblyCopyright("Copyright © HP Inc. 2017")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(False)>
+
+'Die folgende GUID bestimmt die ID der Typbibliothek, wenn dieses Projekt für COM bereitgestellt wird.
+<Assembly: Guid("e6902988-a1c0-402e-ab5a-4e858d0a68ec")>
+
+' Versionsinformationen für eine Assembly bestehen aus den folgenden vier Werten:
+'
+'      Major Version
+'      Minor Version 
+'      Build Number
+'      Revision
+'
+' Sie können alle Werte angeben oder die standardmäßigen Build- und Revisionsnummern 
+' übernehmen, indem Sie "*" wie folgt verwenden:
+' <Assembly: AssemblyVersion("1.0.*")> 
+
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
